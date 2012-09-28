d178e4adcde73b3cf29f8761f6a393b8ff4df5e3
Gowrav Vishwakarma <gowrav@gowrav-acer.(none)>
2012-09-28 09:15:21 +0530

    bit of changes in new registration

--- users/page/newregistration.php
+++ users/page/newregistration.php
@@ -4,18 +4,64 @@ class page_newregistration extends Page {
 		parent::init();
 	
 		$form=$this->add('Form');
-		$form->setModel('Distributor',array('distributor_id','pin','sponsor_id','name',
-							'father/husband_name','date_of_birth','password','retype_password','mobile_no',
-							'address','city','state','pin_code','nominee','relation_with_nominee'),'pan_no',
-							'your_bank','IFSC_code','account_number');
-		// $form->addField('line','distributor_id');
-		// $form->addField('line','pin');
-		// $form->addField('line','sponsor_id');
-		// $form->addField('line','name');
-		// $form->addField('line','father/husband_name');
-		// $form->addField('DatePicker','date_of_birth');
-		// $form->addField('password','password');
-		// $form->addField('line','name');
+		$form->setModel('Distributor',array(
+								'sponsor_id',
+								'pin',
+								'fullname',
+							'Father_HusbandName',
+							'Dob',
+							'Password',
+							'Retype_password',
+							'MobileNo',
+							'Address',
+							'City',
+							'State',
+							'PinCode',
+							'Nominee',
+							'RelationWithNominee',
+							'PanNo',
+							'Bank',
+							'IFSC',
+							'AccountNumber'));
+		$form->addField('dropdown','leg')->setValueList(array(
+												"-1"=>"select leg",
+												"A"=>"A",
+												"B"=>"B"))->set('-1');
+		$form->add('Order')->move('leg','after','sponsor')->now();
+		$form->addField('line','new_id','Distributor ID');
+		$form->addField('line','pin');
+		$form->addSubmit('Submit');
 
+		if($form->isSubmitted()){
+
+			// Check for new joining stopped or not
+			$admin=$this->add('Model_Admin');
+			if($admin->getValue('NewJoinings') != 'Start')
+				$form->js()->univ()->errorMessage("New Joining is stopped by Administrator")->execute();
+
+			if($form->get('leg')=='-1') $form->displayError('leg','Please select any valid leg');
+
+
+			// Sponsor checkings
+			$sponsor=$this->add('Model_Distributor');
+			$sponsor->load($form->get('sponsor_id'));
+
+			$sponsor_leg=$sponsor->ref('Leg');
+			$sponsor_leg->addCondition('Leg',$form->get('leg'));
+			$sponsor_leg->tryLoadAny();
+
+			if($sponsor_leg->loaded()) $form->displayError('leg','This leg is already filled, try another');
+
+			// check pins
+			$pin=$this->add('Model_Pin');
+			$pin->tryLoad($form->get('new_id'));
+			if(!$pin->loaded() OR $pin['Pin'] != strtolower($form->get('pin')) OR $pin['Used']==true)
+				$form->displayError('pin','Pin Validation failed or This is a Used pin');
+
+
+
+			$form->js()->univ()->successMessage('Got it till last')->execute();
+
+		}
 	}
 }
